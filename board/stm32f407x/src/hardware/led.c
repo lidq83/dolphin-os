59340760d1234f0e82fadf77add34a1b05a64458
lidq <lideqiang83@163.com>
2021-03-03 10:13:28 +0800

    开发板为stm32f407ze

--- board/stm32f407x/src/hardware/led.c
+++ board/stm32f407x/src/hardware/led.c
@@ -4,22 +4,11 @@ void led_init()
 {
 	GPIO_InitTypeDef GPIO_InitStructure;
 
-	// RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
-	// GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
-	// GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-	// GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	// GPIO_Init(GPIOD, &GPIO_InitStructure);
-
-	// led_off(0);
-	// led_off(1);
-	// led_off(2);
-	// led_off(3);
-
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOF, ENABLE);
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10;
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOF, &GPIO_InitStructure);
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
 
 	led_off(0);
 	led_off(1);
@@ -30,10 +19,10 @@ void led_off(int led)
 	switch (led)
 	{
 	case 0:
-		GPIO_ResetBits(GPIOF, GPIO_Pin_9);
+		GPIO_ResetBits(GPIOA, GPIO_Pin_6);
 		break;
 	case 1:
-		GPIO_ResetBits(GPIOF, GPIO_Pin_10);
+		GPIO_ResetBits(GPIOA, GPIO_Pin_7);
 		break;
 	default:
 		break;
@@ -45,10 +34,10 @@ void led_on(int led)
 	switch (led)
 	{
 	case 0:
-		GPIO_SetBits(GPIOF, GPIO_Pin_9);
+		GPIO_SetBits(GPIOA, GPIO_Pin_6);
 		break;
 	case 1:
-		GPIO_SetBits(GPIOF, GPIO_Pin_10);
+		GPIO_SetBits(GPIOA, GPIO_Pin_7);
 		break;
 	default:
 		break;
